d1acecdca108ed626774d4e2c0888d39af9b7bcf
Oseias <Oseiasanascimento@hotmail.com>
2018-11-18 16:31:19 -0200

    Componente Mapa criado

---
+++ src/Components/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+ class Map extends React.Component {
+    render (){
+        return (
+            <div id="map" className="map"></div>
+        )
+    }
+}
+
+export default Map;
