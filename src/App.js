a724f3962754936328be6cb4384c4d71962b5f10
Oseias <Oseiasanascimento@hotmail.com>
2018-11-20 12:33:42 -0200

    fix: Infowindow está alternando os marcadores

--- src/App.js
+++ src/App.js
@@ -5,41 +5,49 @@ import Menu from './Components/Menu';
 import './App.css';
 
 class App extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-        locations: [
-            {title: 'Frequência Fit', location: {lat: -23.438605, lng: -46.384569}},
-            {title: '4You Academia', location: {lat: -23.440227, lng: -46.385222}},
-            {title: 'Supermercados X', location: {lat: -23.438605, lng: -46.384144}},
-            {title: 'Mercado Villa Real', location: {lat: -23.441339, lng: -46.383498}}
-        ],
-        map: {},
-        markers: [],
-        infoWindow: {}
+            locations: [
+                {title: 'Frequência Fit', location: {lat: -23.438605, lng: -46.384569}},
+                {title: '4You Academia', location: {lat: -23.440227, lng: -46.385222}},
+                {title: 'Supermercados X', location: {lat: -23.438605, lng: -46.384144}},
+                {title: 'Mercado Villa Real', location: {lat: -23.441339, lng: -46.383498}}
+            ],
+            map: {},
+            markers: []
+        }
     }
-}
 
     componentDidMount () {
-
         window.initMap = this.initMap;
         this.createScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyDVOph5H2ON8d7d3maVA_t9UkTTxkRhuZA&callback=initMap');
+    }
 
+    populateInfoWindow = (marker, infoWindow, map) => {
+       if (infoWindow.marker != marker) {
+           infoWindow.marker = marker;
+           infoWindow.setContent('<div>' + marker.title + '</div>');
+           infoWindow.open(map, marker);
+           // Make sure the marker property is cleared if the infoWindow is closed.
+           infoWindow.addListener('closeclick',function(){
+               infoWindow.setMarker = null;
+           });
+       }
     }
 
     // chamar a função initMap
     initMap = () => {
-
         var map = new google.maps.Map(document.getElementById('map'), {
             center: { lat: -23.440246, lng: -46.3828834},
-            zoom: 16
+            zoom: 16,
+            mapTypeControl: false
         });
 
-        var InfoWindow = new window.google.maps.InfoWindow({});
-        var bounds = new google.maps.LatLngBounds();
-
         this.setState({'map': map});
+
+        var bounds = new google.maps.LatLngBounds();
+        var largeInfowindow = new google.maps.InfoWindow();
 
         for (var i = 0; i < this.state.locations.length; i++) {
           // Get the position from the location array.
@@ -57,19 +65,36 @@ class App extends Component {
 
         this.state.markers.push(marker);
         bounds.extend(this.state.markers[i].position);
+        var infoWindow = new google.maps.InfoWindow();
+
+        marker.addListener('click', function() {
+
+            if (infoWindow.marker !== this) {
+                infoWindow.marker = this;
+
+                infoWindow.setContent('<div>' + this.title + '</div>');
+                infoWindow.open(map, this);
+                // Make sure the marker property is cleared if the infoWindow is closed.
+                infoWindow.addListener('closeclick',function(){
+                    infoWindow.setMarker = null;
+                });
+            }
+        });
 
     }
+}
 
-    }
 
-    render() {
-        return (
-            <section className="container">
-            <Menu />
-            <Map />
-            </section>
-        )
-    }
+
+
+render() {
+    return (
+        <section className="container">
+        <Menu />
+        <Map />
+        </section>
+    )
+}
 
     createScript(src) {
         // Selecionar ponto de referencia
