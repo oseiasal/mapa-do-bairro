7bd788f1468b25ae140a9ac169b22412c7177581
Oseias <Oseiasanascimento@hotmail.com>
2018-11-26 18:49:23 -0200

    Dados separados

---
+++ src/data/UserKey.js
@@ -0,0 +1,1 @@
+export const PrivateKey = 'AIzaSyDVOph5H2ON8d7d3maVA_t9UkTTxkRhuZA';
---
+++ src/data/locations.js
@@ -0,0 +1,36 @@
+export const Places = [{
+    title: 'Frequência Fit',
+    location: {
+        lat: -23.438605,
+        lng: -46.384569
+    },
+    telefone: '(11) 97581-7575'
+}, {
+    title: '4You Academia',
+    location: {
+        lat: -23.440227,
+        lng: -46.385222
+    },
+    telefone: '(11) 98056-7125'
+}, {
+    title: 'Supermercados X',
+    location: {
+        lat: -23.438605,
+        lng: -46.384144
+    },
+    telefone: '(11) 97563-5872'
+}, {
+    title: 'Mercado Villa Real',
+    location: {
+        lat: -23.441339,
+        lng: -46.383498
+    },
+    telefone: '(11) 92147-0022'
+}, {
+    title: 'Canil Municipal',
+    location: {
+        lat: -23.441019,
+        lng: -46.383458
+    },
+    telefone: '(11) 92147-0022'
+}]
