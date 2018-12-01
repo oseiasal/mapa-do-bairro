0868548110b4103de94523de3b9d084ca045950d
Oseias <Oseiasanascimento@hotmail.com>
2018-12-01 01:37:56 -0200

    Código desnecessário removido

--- src/Components/PlaceItem.js
+++ src/Components/PlaceItem.js
@@ -2,30 +2,16 @@ import React from 'react';
 
 export class PlaceItem extends React.Component {
 
-    openInfoWindow(marker, infoWindow, map) {
-        if (infoWindow.marker !== marker) {
-            infoWindow.marker = marker;
-            infoWindow.setContent('<div>' + marker.title + '</div>');
-            infoWindow.open(map, marker);
-            marker.map.setCenter(marker.getPosition());
-            marker.map.panBy(0, 0);
-
-            infoWindow.addListener('closeclick', () => {
-                infoWindow.setMarker = null;
-            });
-        }
-    }
-
-
     callOpenInfoWindow = (context) => {
-        this.openInfoWindow(this.props.marker, this.props.infoWindow, this.props.marker.map);
+        this.props.openInfoWindow(this.props.marker, this.props.infoWindow, this.props.marker.map);
+
     }
 
     render () {
         return (
             <li>
                 <span
-                onClick={this.callOpenInfoWindow.bind()}>
+                onClick={this.callOpenInfoWindow.bind(this)}>
                 {this.props.marker.title}
                 </span>
             </li>
