bb9056568be38924c39f2e601b73217439b6395d
Oseias <Oseiasanascimento@hotmail.com>
2018-12-03 18:08:56 -0200

    refact: Functional stateless

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { PlaceItem } from './PlaceItem';
 import '../App.css';
 
-class Menu extends React.Component {
 
-    render(){
+
+const Menu = ({openMenu, updateQuery, query, marcFiltered, infoWindow, openInfoWindow, markers}) => {
+
         return (
             <div className="menu-option">
                 <div className="search-container">
-                    <div tabIndex="0"  className="menuToggle" onKeyPress={this.props.openMenu} >
+                    <div tabIndex="0"  className="menuToggle" onKeyPress={openMenu} >
                         <input  tabIndex="-1" className="checkbox" type="checkbox"></input>
 
                         <span></span>
@@ -19,24 +20,24 @@ class Menu extends React.Component {
                     </div>
 
                     <input aria-label="Digite o nome do lugar" placeholder="Digite o nome do lugar" className="show-markers showing" type="text"
-                onChange={(event) => {this.props.updateQuery(event.target.value); }}>
+                onChange={(event) => {updateQuery(event.target.value); }}>
                     </input>
 
                     <div className="list showing">
                         <ul>
-                        {this.props.query.length > 0 ? (this.props.marcFiltered.map((marker, index) => {
+                        {query.length > 0 ? (marcFiltered.map((marker, index) => {
                             return <PlaceItem
-                            infoWindow={this.props.infoWindow}
+                            infoWindow={infoWindow}
                             key={index}
                             marker={marker}
-                            openInfoWindow={this.props.openInfoWindow}
+                            openInfoWindow={openInfoWindow}
                             />
-                        })) : (this.props.markers.map((marker, index) => {
+                        })) : (markers.map((marker, index) => {
                             return <PlaceItem
-                            infoWindow={this.props.infoWindow}
+                            infoWindow={infoWindow}
                             key={index}
                             marker={marker}
-                            openInfoWindow={this.props.openInfoWindow}
+                            openInfoWindow={openInfoWindow}
                             />
                         }))}
                         </ul>
@@ -45,6 +46,5 @@ class Menu extends React.Component {
             </div>
         )
     }
-}
 
 export default Menu;
