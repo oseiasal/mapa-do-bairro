d38bad395170c1f655f4ab7e083d96a8b61c7216
Oseias <Oseiasanascimento@hotmail.com>
2018-12-03 17:58:16 -0200

    refact: Stateless Functional

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
- class Map extends React.Component {
-    render (){
-        return (
-            <main>
-                <div id="map" role="application" aria-label="Mapa da Cidade de Guarulhos" className="map"></div>
-            </main>        
-)
-    }
+const Map = () => {
+
+    return (
+        <main>
+            <div id="map" role="application" aria-label="Mapa da Cidade de Guarulhos" className="map"></div>
+        </main>
+     )
 }
 
 export default Map;
