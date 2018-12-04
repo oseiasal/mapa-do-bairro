0e50b49fa265202505739590349440d9425bd5ab
Oseias <Oseiasanascimento@hotmail.com>
2018-12-04 00:49:17 -0200

    Updates

---
+++ precache-manifest.e4fc9a6157a8e5d95f50f4afd53e07f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b2a6a2c265745a6032a",
+    "url": "/mapa-do-bairro/static/js/runtime~main.6b2a6a2c.js"
+  },
+  {
+    "revision": "d996f0acf5d61ce1c849",
+    "url": "/mapa-do-bairro/static/js/main.d996f0ac.chunk.js"
+  },
+  {
+    "revision": "d9e34ced1a4223cee0ea",
+    "url": "/mapa-do-bairro/static/js/1.d9e34ced.chunk.js"
+  },
+  {
+    "revision": "d996f0acf5d61ce1c849",
+    "url": "/mapa-do-bairro/static/css/main.19322443.chunk.css"
+  },
+  {
+    "revision": "7eb085d761d8943eb72b704ad0d994f1",
+    "url": "/mapa-do-bairro/index.html"
+  }
+];
